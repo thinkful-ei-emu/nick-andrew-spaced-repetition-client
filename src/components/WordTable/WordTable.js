8df583bcf7844c79fdce27efc8016750bb9e1a9f
Nicholas Gunter <ygnick@live.com>
2019-09-09 16:03:40 -0400

    fetch for language data and skeleton of word table

---
+++ src/components/WordTable/WordTable.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+class WordTable extends React.Component {
+
+
+
+
+  render(){
+
+
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default WordTable;
--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,6 +1,41 @@
 import React, { Component } from 'react'
+import config from '../../config';
+import TokenService from '../../services/token-service';
+import WordTable from '../../components/WordTable/WordTable';
 
 class DashboardRoute extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      error: null
+    }
+  }
+
+  async componentDidMount(){
+
+    try {
+    const data = await fetch(`${config.API_ENDPOINT}/language`, {
+      headers: {
+        'authorization': `Bearer ${TokenService.getAuthToken()}`
+      }
+    })
+
+    const languageData = await data.json()
+
+    console.log(languageData);
+    }
+
+    catch(e){
+      this.setState({
+        error: e
+      })
+    }
+
+  }
+
+
+
   render() {
     return (
       <section>
