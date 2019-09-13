907466f3380c040a614da6fbb7bf7d11f61b0959
Nicholas Gunter <ygnick@live.com>
2019-09-13 16:07:50 -0400

    final touch ups

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 export default {
-  API_ENDPOINT:'http://localhost:8000/api',
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 };
 
 
-// process.env.REACT_APP_API_ENDPOINT || 
